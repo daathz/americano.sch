86dd9e5bb81e6a1121be4417e8c418cb66781d95
daathz <novk.viktor@gmail.com>
2019-03-25 15:31:52 +0100

    Events can't overlap each other

--- middlewares/event/createEvent.js
+++ middlewares/event/createEvent.js
@@ -14,15 +14,21 @@ module.exports = (objRepo) => {
     }
 
     if (req.body.startevent < req.body.endevent) {
-      eventModel.create({
-        start: req.body.startevent,
-        end: req.body.endevent
-      }, (err) => {
-        if (err) return next(err)
-        return res.redirect('/events')
+      eventModel.findOne({
+        start: {$lte: req.body.endevent},
+        end: {$gte: req.body.startevent}
+      }, (err, event) => {
+        if (err || event) return next()
+        else {
+          eventModel.create({
+            start: req.body.startevent,
+            end: req.body.endevent
+          }, (err) => {
+            if (err) return next(err)
+            res.redirect('/events')
+          })
+        }
       })
-    } else {
-      return next()
-    }
+    } else return next()
   }
 }
